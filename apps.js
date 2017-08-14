a8d860208922d7dc968ba0a91cb0eae223a6ebe4
saraharless <sara.e.harless@gmail.com>
2017-08-14 15:09:20 -0500

    completed assignment using ternary operators

--- apps.js
+++ apps.js
@@ -4,16 +4,16 @@ const moment = require('moment');
 let now = moment().format('dddd MMM DD, YYYY h:m:s');
 let today = moment().format('DDDD');
 let secs = moment().format('ssss');
-let dayst = moment().isDST();
+let dayst = (moment().isDST() ? 'is' : 'is not');
+let leapy = (moment().isLeapYear() ? 'is' : 'is not');
 
-let dstcheck =
-
-
+// testing logs
 console.log('ghost toast');
 console.log('it is ' + now);
 console.log(chalk.blue('boo'));
-
-console.log('it is ' + chalk.blue(now));
-console.log('it is the ' + chalk.magenta(today) + ' day of the year');
-console.log('it is ' + chalk.cyan(secs) + ' seconds into the day');
-console.log('it ' + chalk.green(dayst) + ' Daylight Savings Time');
+// assignment logs
+console.log('it is ' + chalk.blue(now) + '.');
+console.log('it is the ' + chalk.magenta(today) + 'th day of the year' + '.');
+console.log('it is ' + chalk.cyan(secs) + ' seconds into the day' + '.');
+console.log('it ' + chalk.green(dayst) + ' Daylight Savings Time' + '.');
+console.log('it ' + chalk.red(leapy) + ' a Leap Year' + '.');
