28d0d9105755fdf9ba91d88c8f0a4d2ba62da881
saraharless <sara.e.harless@gmail.com>
2017-08-14 14:18:00 -0500

    initial

---
+++ apps.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk');
+const moment = require('moment');
+// moment().format();
+let now = moment().format();
+
+
+console.log('ghost toast');
+console.log('it is' + now);
+console.log(chalk.blue('boo'));
+
+console.log('it is ' + now);
